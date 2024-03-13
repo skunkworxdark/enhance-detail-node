39aa99e8e1a848a552a7a4ab4add5eea052477f8
skunkworxdark <neil_hodgson@hotmail.com>
2024-03-13 18:36:10 +0000

    First Checkin

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# enhance-detail-node
- enhance detail node for InvokeAI
+# `enhance_detail` for InvokeAI (v4.0+)
+A single node that can enhance the detail in an image. Increase or decrease details in an image using a guided filter (as opposed to the typical Gaussian blur used by most sharpening filters.) Based on the `Enhance Detail` ComfyUI node from  https://github.com/spacepxl/ComfyUI-Image-Filters
+
+
+![alt text](images/Node.png)
+
+`Filter Radius` int(1-100) adjusts how many pixels will be considered when filtering to find detail and denoise. A larger number when reducing detail will increase the smoothing effect. Also, a large denoise with a large radius will have a blurring effect.
+
+`Sigma` Float(0.01-100) This is the EPS parameter of the Guided Filter routine. This has varied effects from not much to a lot depending on how much the other settings are generally the higher the other settings are the more this has an effect. I've not done enough testing to quantify it. 
+
+`Denoise` Float(0-10) This is part of a bilateral filter to reduce noise in the image. High values will have the effect of blurring the image. A value of 0 will skip the Bilateral Filtering. 
+
+`Detail Multiplier` Float(0-100) This has the most effect. Values 1 to 3 are normally the most presentable at increasing detail. Values under 1 will reduce the detail.
+
+
+## Usage
+### <ins>Install</ins><BR>
+There are two options to install the nodes:
+
+1. **Recommended**: Git clone into the `invokeai/nodes` directory. This allows updating via `git pull`.
+
+    In the InvokeAI nodes folder, run:
+    ```bash
+    git clone https://github.com/skunkworxdark/enhance-detail-node.git
+    ```
+
+2. Manually download [enhance_detail.py](enhance_detail.py) & [__init__.py](__init__.py) then place them in a subfolder under `invokeai/nodes`. 
+
+### <ins>Update</ins><BR>
+Run a `git pull` from the `enhance-detail-node` folder.
+
+Or run `update.bat`(windows) or `update.sh`(Linux).
+
+For manual installs, download and replace the files.
+
+### <ins>Remove</ins><BR>
+Delete the `enhance-detail-node` folder. Or rename it to `_enhance-detail-node` so InvokeAI will ignore it.
+
+## TODO
+Example workflows and screenshots
+
+## Examples
+![alt text](images/Comparison.png)
+input:
+![alt text](images/Input.png)
+Less Detail (Detail Multiplier 0.2)
+![alt text](images/Output-lessdetail.png)
+Detail 2
+![alt text](images/Output-Detail2.png)
+Detail 3
+![alt text](images/Outpu-Detail3.png)
---
+++ enhance_detail.py
@@ -0,0 +1,92 @@
+# 2024 skunkworxdark (https://github.com/skunkworxdark)
+# Based on the `Enhance Detail` ComfyUI node from  https://github.com/spacepxl/ComfyUI-Image-Filters
+
+import cv2
+import numpy as np
+from cv2.ximgproc import guidedFilter
+from PIL import Image
+
+from invokeai.app.invocations.baseinvocation import (
+    BaseInvocation,
+    InvocationContext,
+    invocation,
+)
+from invokeai.app.invocations.fields import InputField, WithBoard, WithMetadata
+from invokeai.app.invocations.primitives import (
+    ImageField,
+    ImageOutput,
+)
+
+
+@invocation(
+    "enhance_detail",
+    title="Enhance Detail",
+    tags=["enhance", "detail", "image"],
+    category="image",
+    version="1.0.0",
+)
+class EnhanceDetailInvocation(BaseInvocation, WithMetadata, WithBoard):
+    """Enhance Detail using guided filter"""
+
+    # Inputs
+    image: ImageField = InputField(description="The image to detail enhance")
+    filter_radius: int = InputField(
+        default=2,
+        ge=1,
+        le=64,
+        description="radius of filter",
+    )
+    sigma: float = InputField(
+        default=0.1,
+        ge=0.0,
+        le=100.0,
+        description="sigma",
+    )
+    denoise: float = InputField(
+        default=0.1,
+        ge=0.0,
+        le=10.0,
+        description="denoise",
+    )
+    detail_multiplier: float = InputField(
+        default=2.0,
+        ge=0.0,
+        le=100.0,
+        description="detail multiplier",
+    )
+
+    def invoke(self, context: InvocationContext) -> ImageOutput:
+        source = context.images.get_pil(self.image.image_name)
+        source_rgb = source.convert(mode="RGB")
+
+        # Check for zero filter radius and return original image
+        if self.filter_radius == 0:
+            return ImageOutput(image=self.image, width=source_rgb.width, height=source_rgb.height)
+
+        # Convert to NumPy array 0..1
+        np_img = np.array(source_rgb).astype(np.float32) / 255
+
+        # Apply bilateral filtering for denoising (if denoise > 0.0)
+        rad = self.filter_radius * 2 + 1
+        denoise = self.denoise / 10
+        imgB = cv2.bilateralFilter(np_img, rad, denoise, rad).astype(np.float32) if denoise > 0.0 else np_img
+
+        # Apply guided filtering
+        sig = self.sigma / 10
+        imgG = np.clip(guidedFilter(np_img, np_img, rad, sig), 0.001, 1).astype(np.float32)
+
+        # Apply detail enhancement and covert back to 0..255
+        details = (imgB / imgG - 1) * self.detail_multiplier + 1
+        output_array = (np.clip(details * imgG - imgB + np_img, 0, 1) * 255).astype(np.uint8)
+
+        # Output Image
+        output_image = Image.fromarray(output_array)
+        # reapply original mode and Alpha channel if needed
+        if "A" in source.mode:
+            output_image.putalpha(source.getchannel("A"))
+
+        if not (output_image.mode == source.mode):
+            output_image.convert(source.mode)
+
+        image_dto = context.images.save(output_image)
+        return ImageOutput.build(image_dto)
